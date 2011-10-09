9bdd06c37b44521e7144cc3765056a14d65b7b56
Libor Kramolis <libor.kramolis@gmail.com>
2011-10-09 14:09:23 +0200

    Parse notation.

---
+++ android/src/cz/kramolis/gdd2011/android/MusicNotation.java
@@ -0,0 +1,103 @@
+package cz.kramolis.gdd2011.android;
+
+/**
+ * Allowed note characters are {@code cCdDefFgGabh}, Use number 2 after character to distinguish the second octave.
+ * Use character {@code |} (pipe) as rest (pause).
+ *
+ * @author Libor Kramolis
+ */
+public class MusicNotation {
+
+	private static final String TAG = "MusicNotation";
+
+	private static final String SUPPORTED_CHARS = "cCdDefFgGabh";
+	private static final String SUPPORTED_NUMBERS = "12";
+	private static final char REST_CHAR = '|';
+
+	private final String notation;
+
+	private MusicNotation(String notation) {
+		this.notation = notation;
+	}
+
+	//
+	// get
+	//
+
+	public String getNotation() {
+		return notation;
+	}
+
+
+	//
+	// utils
+	//
+
+	/**
+	 * Lookups for music notation in {@param text}. Notation is bordered by brackets ({@code [} and {@code ]}).
+	 * If there are no brackets it returns {@code null}. Otherwise it returns text between brackets with following modifications:
+	 * <ul>
+	 * <li>all white spaces are removed</li>
+	 * </ul>
+	 * If there is no text between brackets it returns empty string.
+	 *
+	 * @param text
+	 * @return mentioned music notation in {@param text}
+	 * @throws IllegalArgumentException if there are brackets with wrong order or if notation beaks syntax.
+	 */
+	public static MusicNotation lookup(String text) throws IllegalArgumentException {
+		if (text == null) {
+			throw new IllegalArgumentException("Wrong format: text is mandatory");
+		}
+
+		int startIndex = text.indexOf('[');
+		int endIndex = text.indexOf(']');
+		{
+			if ((startIndex == -1) && (endIndex == -1)) {
+				return null;
+			}
+			if (startIndex > endIndex) {
+				throw new IllegalArgumentException("Wrong format: wrong order of brackets");
+			}
+			if (startIndex == -1) {
+				throw new IllegalArgumentException("Wrong format: start bracket is missing.");
+			}
+			if (endIndex == -1) {
+				throw new IllegalArgumentException("Wrong format: end bracket is missing.");
+			}
+		}
+
+		text = text.substring(startIndex + 1, endIndex).trim();
+		{
+			text = text.replaceAll("\\s", "");
+//			Log.d(TAG, "Clean text: " + text);
+		}
+		StringBuilder sb = new StringBuilder();
+		{
+			boolean charLast = false;
+			for (int i = 0; i < text.length(); i++) {
+				char ch = text.charAt(i);
+
+				if ((REST_CHAR == ch) || (SUPPORTED_CHARS.indexOf(ch) != -1)) {
+					if (charLast) {
+						sb.append('1');
+					}
+					sb.append(ch);
+					if (i + 1 == text.length()) {
+						sb.append('1');
+					}
+					charLast = true;
+				} else if ((SUPPORTED_NUMBERS.indexOf(ch) != -1) && charLast) {
+					sb.append(ch);
+					charLast = false;
+				} else {
+					throw new IllegalArgumentException("Illegal character '" + ch + "'");
+				}
+			}
+		}
+		MusicNotation musicNotation = new MusicNotation(sb.toString());
+
+		return musicNotation;
+	}
+
+}
---
+++ android/test/cz/kramolis/gdd2011/android/MusicNotationTest.java
@@ -0,0 +1,72 @@
+package cz.kramolis.gdd2011.android;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * @author Libor Kramolis
+ */
+public class MusicNotationTest {
+
+	@Test
+	public void testLookup() throws Exception {
+		{
+			testLookupImpl(null, "");
+			testLookupImpl(null, " aaaaaaaa ");
+			testLookupImpl(null, " cdefgab|cdefgab ");
+
+			testLookupImpl("", "[]");
+			testLookupImpl("", "[	]");
+			testLookupImpl("", "[ ]");
+			testLookupImpl("", "  []	");
+			testLookupImpl("", "  #abc  []");
+			testLookupImpl("", " [] #abc  ");
+			testLookupImpl("", "  #abc [] ");
+			testLookupImpl("c1", "[c]");
+			testLookupImpl("c1", "[c1]");
+			testLookupImpl("c2", "[c2]");
+			testLookupImpl("c1c1", "[cc]");
+			testLookupImpl("c1c1c1c2c1c1c2", "[cc1cc2ccc2]");
+			testLookupImpl("d1d1d1", "  #abc	[d1dd]	#opq  ");
+			testLookupImpl("e1e1e1", "  [ee1e] #abc	#opq  #xyz	");
+			testLookupImpl("f1f1f1", " [fff1] [ggg] #lapardon ");
+			testLookupImpl("a1a1a1|1b1b1b1", "[a a a |	b	b	b ]");
+			testLookupImpl("c1C1d1D1e1f1F1g1G1a1b1h1", "[cCdDefFgGabh]");
+			testLookupImpl("c1C1d1D1e1f1F1g1G1a1b1h1", "[c1C1d1D1e1f1F1g1G1a1b1h1]");
+			testLookupImpl("c2C2d2D2e2f2F2g2G2a2b2h2", "[c2C2d2D2e2f2F2g2G2a2b2h2]");
+			testLookupImpl("|1c1|1C1|1d1|1D1|1e1|1f1|1F1|1g1|1G1|1a1|1b1|1h1|1|1c2|1C2|1d2|1D2|1e2|1f2|1F2|1g2|1G2|1a2|1b2|1h2|1|1",
+					"[|c|C|d|D|e|f|F|g|G|a|b|h||c2|C2|d2|D2|e2|f2|F2|g2|G2|a2|b2|h2||]");
+		}
+		{
+			testLookupThrowsException(null);
+			testLookupThrowsException("[");
+			testLookupThrowsException("]");
+			testLookupThrowsException("][");
+			testLookupThrowsException(" aaa ] bbb [ ccc ");
+
+			testLookupThrowsException("[1]");
+			testLookupThrowsException("[2]");
+			testLookupThrowsException("[z]");
+			testLookupThrowsException("[c3]");
+		}
+	}
+
+	private void testLookupImpl(String expected, String text) {
+		MusicNotation musicNotation = MusicNotation.lookup(text);
+		if (expected == null) {
+			Assert.assertNull(musicNotation);
+		} else {
+			Assert.assertEquals(expected, musicNotation.getNotation());
+		}
+	}
+
+	private void testLookupThrowsException(String text) {
+		try {
+			MusicNotation.lookup(text);
+			Assert.fail("Must fail: " + text);
+		} catch (IllegalArgumentException ex) {
+			//OK
+		}
+	}
+
+}
